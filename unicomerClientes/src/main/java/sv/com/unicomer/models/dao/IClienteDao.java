78a6673e6e8d2fee14cd6cd395797ff0e62fcaaf
Carlos Ernesto Menendez Roldan <ernesto_menendez@unicomer.com>
2022-09-24 19:28:26 -0600

    pruebaJava

--- unicomerClientes/src/main/java/sv/com/unicomer/models/dao/IClienteDao.java
+++
@@ -1,7 +0,0 @@
-package sv.com.unicomer.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import sv.com.unicomer.models.entity.Cliente;
-
-public interface IClienteDao extends CrudRepository<Cliente, Long>{}
